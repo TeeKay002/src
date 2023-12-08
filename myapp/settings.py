fe8acbabc7238609d4d4626026ab7f9d310684c7
TeeKay002 <105482782+TeeKay002@users.noreply.github.com>
2023-12-08 09:20:13 +0800

    resolve the templateDoesntExist error

--- myapp/settings.py
+++ myapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-4suq&@wf866*brcdq@9u73th#8yvv#a#dtj#@e@k8^xcu1#w*f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quantumsphere.47to.xyz']
+ALLOWED_HOSTS = ['quantumsphere.47to.xyz', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'greetings'
 ]
 
 MIDDLEWARE = [
